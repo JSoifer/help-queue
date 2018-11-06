7612bf308696e5fd94a49533e7340cd5a7a39ab4
Julianne Soifer <juliannesoifer@gmail.com>
2018-11-06 07:19:01 -0800

    create help queue project files and set up environment

---
+++ react-help-queue/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './Ticketlist';
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+
+    </div>
+  );
+}
+
+export default App;
