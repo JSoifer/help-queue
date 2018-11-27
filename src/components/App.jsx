b7845fa88c8b64d2a7e0f400e3e9ad5cad89265c
Student <example@example.com>
2018-11-27 14:19:43 -0800

    add multiple reducers and combine, refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,15 +12,6 @@ import { connect } from 'react-redux';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    console.log(props);
-    this.state = {
-      selectedTicket: null
-    };
-    this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
-  }
-
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
     this.updateTicketElapsedWaitTime(),
@@ -38,12 +29,6 @@ class App extends React.Component {
     // this.setState({masterTicketList: newMasterTicketList});
   }
 
-  handleChangingSelectedTicket(ticketId){
-    this.setState({selectedTicket: ticketId});
-  }
-
-
-
   render(){
     return (
       <div>
@@ -51,21 +36,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.props.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl />} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}
-            onTicketSelection={this.handleChangingSelectedTicket}
-            selectedTicket={this.state.selectedTicket}/>} />
+          <Route path='/admin' render={(props)=><Admin currentRouterPath={props.location.pathname}/>} />
           <Route component={Error404} />
         </Switch>
       </div>
     );
   }
 }
+
 App.propTypes = {
   masterTicketList: PropTypes.object
 };
+
 const mapStateToProps = state => {
   return {
-    masterTicketList: state
-  }
-}
+    masterTicketList: state.masterTicketList
+  };
+};
+
 export default withRouter(connect(mapStateToProps)(App));
